d75b47db93716883a04063eab891d737e1fff944
Kevin CATHALY <alakme@gmail.com>
2015-01-05 14:32:15 +0100

    add failing test

--- tactestTests/tactestTests.m
+++ tactestTests/tactestTests.m
@@ -30,6 +30,10 @@
     XCTAssert(YES, @"Pass");
 }
 
+- (void)testFail {
+    XCTAssertTrue(NO, @"FAILLLLLLLLL");
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
